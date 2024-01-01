6163e0c7404b9b97c8eb6e7e135d1f3184d999fe
SAEED.PANAHI <panahi.projects@gmail.com>
2024-01-01 19:00:42 +0330

    Install react-hook-form & zod, implement signin page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -12,15 +12,27 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { useForm } from "react-hook-form";
 
-export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+interface SignInForm {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
+
+export default function SignInPage() {
+  const { register, handleSubmit } = useForm<SignInForm>();
+
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     email: data.get("email"),
+  //     password: data.get("password"),
+  //   });
+  // };
+  const onSubmitForm = (data: SignInForm) => {
+    console.log({ data });
   };
 
   return (
@@ -40,29 +52,33 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmitForm)}
+          sx={{ mt: 1 }}
+        >
           <TextField
             margin="normal"
             required
             fullWidth
             id="email"
             label="Email Address"
-            name="email"
             autoComplete="email"
             autoFocus
+            {...register("email")}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="password"
             label="Password"
             type="password"
             id="password"
             autoComplete="current-password"
+            {...register("password")}
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
+            control={<Checkbox color="primary" {...register("rememberMe")} />}
             label="Remember me"
           />
           <Button
